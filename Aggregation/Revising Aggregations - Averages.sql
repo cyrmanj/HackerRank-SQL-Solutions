e71c782fb0276e8f13b85a46ddd28a30ad2073cd
Abraham <57534337+cyrmanj@users.noreply.github.com>
2026-07-12 08:43:05 -0600

    Create SQL query for average population aggregation
    Add SQL query to calculate average population in California.

---
+++ Aggregation/Revising Aggregations - Averages.sql
@@ -0,0 +1,9 @@
+-- Query name: Revising Aggregations - Averages
+-- Subdomain: Advanced Select
+-- Difficulty: Easy 
+-- Concepts: Select, Aggregations
+-- Platform: HackerRank
+
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
